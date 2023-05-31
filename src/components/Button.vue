ca20a260a70d3988789136cbe4dd526f060acc07
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-05-31 16:51:21 -0600

    create Header and Button components

---
+++ src/components/Button.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- v-on:click="onClick()" === @click for adding an event -->
+  <button @click="onClick()" v-bind:style="{background: color}" class="btn">{{ text  }}</button>
+</template>
+
+<script>
+  export default {
+    /* eslint-disable */
+    name: 'Button',
+    props: {
+      text: String,
+      color: String
+    }, 
+    methods: {
+      // adding a method when an event is defined
+      onClick() {
+        console.log("click")
+      }
+    }
+  }
+
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button text="Add Task" color="green"/>
+  </header>
+</template>
+
+
+<script>
+import Button from './Button';
+
+export default {
+  /* eslint-disable */
+  name: 'Header',
+  props: {
+    title: String
+  },
+  components: {
+    Button
+  }
+};
+</script>
+
+
+<!-- scoped means any styling is only applied to this particular component -->
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
